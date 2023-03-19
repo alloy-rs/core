a6cde026a0e54303f28972f8c8fdb56832356f6c
James <james@prestwi.ch>
2023-03-19 15:50:00 -0700

    refactor: trim error type

--- abi/src/errors.rs
+++ abi/src/errors.rs
@@ -18,13 +18,10 @@ use thiserror::Error;
 /// ABI result type
 pub type AbiResult<T> = core::result::Result<T, Error>;
 
-/// ABI errors
+/// ABI Encoding and Decoding errors.
 #[cfg_attr(feature = "std", derive(Error))]
 #[derive(Debug)]
 pub enum Error {
-    /// Invalid entity such as a bad function name.
-    #[cfg_attr(feature = "std", error("Invalid name: {0}"))]
-    InvalidName(String),
     /// Invalid data.
     #[cfg_attr(feature = "std", error("Invalid data"))]
     InvalidData,
@@ -34,30 +31,7 @@ pub enum Error {
     #[cfg_attr(feature = "std", error("Reserialization did not match original"))]
     /// Validation reserialization did not match input
     ReserMismatch,
-    /// Serialization error.
-    #[cfg(feature = "full-serde")]
-    #[error("Serialization error: {0}")]
-    SerdeJson(#[from] serde_json::Error),
-    /// Integer parsing error.
-    #[cfg(feature = "serde")]
-    #[cfg_attr(feature = "std", error("Integer parsing error: {0}"))]
-    ParseInt(#[cfg_attr(feature = "std", from)] num::ParseIntError),
-    /// Hex string parsing error.
-    #[cfg(feature = "serde")]
-    #[cfg_attr(feature = "std", error("Hex parsing error: {0}"))]
-    Hex(#[cfg_attr(feature = "std", from)] hex::FromHexError),
     /// Other errors.
     #[cfg_attr(feature = "std", error("{0}"))]
     Other(Cow<'static, str>),
-}
-
-#[cfg(feature = "serde")]
-impl From<uint::FromDecStrErr> for Error {
-    fn from(err: uint::FromDecStrErr) -> Self {
-        use uint::FromDecStrErr::*;
-        match err {
-            InvalidCharacter => Self::Other(Cow::Borrowed("Uint parse error: InvalidCharacter")),
-            InvalidLength => Self::Other(Cow::Borrowed("Uint parse error: InvalidLength")),
-        }
-    }
 }
