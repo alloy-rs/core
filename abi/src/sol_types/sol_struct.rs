30746f3d5e0e6683ef5a54643fb805c594caddc8
James <james@prestwi.ch>
2023-04-13 17:55:09 -0700

    feature: encode_eip712

--- abi/src/sol_types/sol_struct.rs
+++ abi/src/sol_types/sol_struct.rs
@@ -5,7 +5,7 @@ use ethers_primitives::B256;
 
 use crate::{util::keccak256, SolType};
 
-use crate::no_std_prelude::*;
+use crate::{no_std_prelude::*, Eip712Domain};
 
 type TupleFor<T> = <T as SolStruct>::Tuple;
 type TupleTokenTypeFor<T> = <TupleFor<T> as SolType>::TokenType;
@@ -69,6 +69,20 @@ pub trait SolStruct {
         let mut type_hash = self.type_hash().to_vec();
         type_hash.extend(self.encode_data());
         keccak256(type_hash)
+    }
+
+    /// EIP-712 `signTypedData`
+    /// <https://eips.ethereum.org/EIPS/eip-712#specification-of-the-eth_signtypeddata-json-rpc>
+    fn encode_eip712(&self, domain: &Eip712Domain) -> B256 {
+        let domain_separator = domain.hash_struct();
+        let struct_hash = self.hash_struct();
+
+        let mut digest_input = [0u8; 2 + 32 + 32];
+        digest_input[0] = 0x19;
+        digest_input[1] = 0x01;
+        digest_input[2..34].copy_from_slice(&domain_separator[..]);
+        digest_input[34..66].copy_from_slice(&struct_hash[..]);
+        keccak256(digest_input)
     }
 }
 
