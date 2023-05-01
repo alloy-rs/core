5db6b427e5f98401193f1e3a3978fb92edf39179
James <james@prestwi.ch>
2023-05-01 10:03:11 -0500

    clean: trim parity function impls

--- primitives/src/bits/fixed.rs
+++ primitives/src/bits/fixed.rs
@@ -36,7 +36,6 @@ impl<'a, const N: usize> From<&'a [u8; N]> for FixedBytes<N> {
     ///
     /// The given bytes are interpreted in big endian order.
     #[inline]
-    #[track_caller]
     fn from(bytes: &'a [u8; N]) -> Self {
         FixedBytes(*bytes)
     }
@@ -50,7 +49,6 @@ impl<'a, const N: usize> From<&'a mut [u8; N]> for FixedBytes<N> {
     ///
     /// The given bytes are interpreted in big endian order.
     #[inline]
-    #[track_caller]
     fn from(bytes: &'a mut [u8; N]) -> Self {
         FixedBytes(*bytes)
     }
@@ -109,76 +107,53 @@ impl<const N: usize> FixedBytes<N> {
 
     /// Returns a new fixed hash where all bits are set to the given byte.
     #[inline]
-    #[track_caller]
     pub const fn repeat_byte(byte: u8) -> FixedBytes<N> {
         FixedBytes([byte; N])
     }
     /// Returns a new zero-initialized fixed hash.
     #[inline]
-    #[track_caller]
     pub const fn zero() -> FixedBytes<N> {
         FixedBytes::repeat_byte(0u8)
     }
     /// Returns the size of this hash in bytes.
     #[inline]
-    #[track_caller]
     pub const fn len_bytes() -> usize {
-        32
+        N
     }
     /// Extracts a byte slice containing the entire fixed hash.
     #[inline]
-    #[track_caller]
     pub const fn as_bytes(&self) -> &[u8] {
         &self.0
     }
     /// Extracts a mutable byte slice containing the entire fixed hash.
     #[inline]
-    #[track_caller]
     pub fn as_bytes_mut(&mut self) -> &mut [u8] {
         &mut self.0
     }
     /// Extracts a reference to the byte array containing the entire fixed hash.
     #[inline]
-    #[track_caller]
     pub const fn as_fixed_bytes(&self) -> &[u8; N] {
         &self.0
     }
     /// Extracts a reference to the byte array containing the entire fixed hash.
     #[inline]
-    #[track_caller]
     pub fn as_fixed_bytes_mut(&mut self) -> &mut [u8; N] {
         &mut self.0
     }
     /// Returns the inner bytes array.
     #[inline]
-    #[track_caller]
     pub const fn to_fixed_bytes(self) -> [u8; N] {
         self.0
     }
     /// Returns a constant raw pointer to the value.
     #[inline]
-    #[track_caller]
     pub const fn as_ptr(&self) -> *const u8 {
         self.as_bytes().as_ptr()
     }
     /// Returns a mutable raw pointer to the value.
     #[inline]
-    #[track_caller]
     pub fn as_mut_ptr(&mut self) -> *mut u8 {
         self.as_bytes_mut().as_mut_ptr()
-    }
-    /// Assign the bytes from the byte slice `src` to `self`.
-    ///
-    /// # Note
-    ///
-    /// The given bytes are interpreted in big endian order.
-    ///
-    /// # Panics
-    ///
-    /// If the length of `src` and the number of bytes in `self` do not match.
-    pub fn assign_from_slice(&mut self, src: &[u8]) {
-        assert_eq!(src.len(), 32);
-        self.as_bytes_mut().copy_from_slice(src);
     }
     /// Create a new fixed-hash from the given slice `src`.
     ///
@@ -189,21 +164,20 @@ impl<const N: usize> FixedBytes<N> {
     /// # Panics
     ///
     /// If the length of `src` and the number of bytes in `Self` do not match.
+    #[track_caller]
     pub fn from_slice(src: &[u8]) -> Self {
         assert_eq!(src.len(), 32);
         let mut ret = Self::zero();
-        ret.assign_from_slice(src);
+        ret.copy_from_slice(src);
         ret
     }
     /// Returns `true` if all bits set in `b` are also set in `self`.
     #[inline]
-    #[track_caller]
     pub fn covers(&self, b: &Self) -> bool {
         &(*b & *self) == b
     }
     /// Returns `true` if no bits are set.
     #[inline]
-    #[track_caller]
     pub fn is_zero(&self) -> bool {
         self.as_bytes().iter().all(|&byte| byte == 0u8)
     }
