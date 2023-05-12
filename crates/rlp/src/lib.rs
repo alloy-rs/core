1d58efab12de1e9c234379f9a2e4fd5a09defe53
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-05-12 23:41:22 +0200

    fix: gate rlp tests to alloc

--- crates/rlp/src/lib.rs
+++ crates/rlp/src/lib.rs
@@ -11,6 +11,11 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
+// Used in alloc tests.
+#[cfg(test)]
+#[allow(unused_extern_crates)]
+extern crate hex_literal;
+
 mod decode;
 mod encode;
 mod types;
