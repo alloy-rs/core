9e332a51eb5b6b33f0bfd795b22702e64ae738b7
James <james@prestwi.ch>
2023-06-08 23:48:01 -0700

    fix: event trait fixes

--- crates/sol-types/src/types/event.rs
+++ crates/sol-types/src/types/event.rs
@@ -1,5 +1,6 @@
-use crate::{no_std_prelude::*, token::TokenSeq, Result, SolType};
 use alloy_primitives::B256;
+
+use crate::{token::TokenSeq, SolType};
 
 /// Solidity event.
 ///
@@ -9,59 +10,133 @@ use alloy_primitives::B256;
 /// using the [`sol`][crate::sol] proc macro to parse a Solidity event
 /// definition.
 pub trait SolEvent: Sized {
-    /// The underlying tuple type which represents this type's members.
+    /// The underlying tuple type which represents this event's non-indexed and
+    /// dynamically-sized event parameters. These parameters are ABI encoded
+    /// and included in the log body.
     ///
-    /// If this type has no arguments, this will be the unit type `()`.
-    type Tuple: SolType<TokenType = Self::Token>;
+    /// If this event has no non-indexed and no dynamically-sized parameters,
+    /// this will be the unit type `()`.
+    type BodyTuple: SolType<TokenType = Self::BodyToken>;
 
-    /// The corresponding [TokenSeq] type.
-    type Token: TokenSeq;
+    /// The [`TokenSeq`] type corresponding to the tuple.
+    type BodyToken: TokenSeq;
 
     /// The event's ABI signature.
     const SIGNATURE: &'static str;
 
-    /// The event's first topic: `keccak256(SIGNATURE)`
-    const TOPIC_ZERO: B256;
+    /// The keccak256 hash of the event's ABI signature. For non-anonymous
+    /// events, this will be the topic0 of the event.
+    const SIGNATURE_HASH: [u8; 32];
+
+    /// True if the event is anonymous.
+    const ANONYMOUS: bool;
+
+    /// The number of topics.
+    const TOPICS_LEN: usize;
+
+    /// Type of Topic 0. This is typically a [`B256`] representing the event's
+    /// signature. However, for anonymous events, this may be some other type.
+    /// If an anonymous event has no indexed parameters, this will be the unit
+    /// type `()`.
+    type Topic0: SolType;
+    /// Type of Topic 1. If the event does not have an indexed parameter at this
+    /// position, this will be the unit type `()`.
+    type Topic1: SolType;
+    /// Type of Topic 2. If the event does not have an indexed parameter at this
+    /// position, this will be the unit type `()`.
+    type Topic2: SolType;
+    /// Type of Topic 3. If the event does not have an indexed parameter at this
+    /// position, this will be the unit type `()`.
+    type Topic3: SolType;
 
     /// Converts to the tuple type used for ABI encoding and decoding.
-    fn to_rust(&self) -> <Self::Tuple as SolType>::RustType;
+    fn body_to_rust(&self) -> <Self::BodyTuple as SolType>::RustType;
 
-    /// Convert from the tuple type used for ABI encoding and decoding.
-    fn from_rust(tuple: <Self::Tuple as SolType>::RustType) -> Self;
+    fn from_rust(
+        topic0: <Self::Topic0 as SolType>::RustType,
+        topic1: <Self::Topic1 as SolType>::RustType,
+        topic2: <Self::Topic2 as SolType>::RustType,
+        topic3: <Self::Topic3 as SolType>::RustType,
+        body: <Self::BodyTuple as SolType>::RustType,
+    ) -> Self;
 
-    /// The size of the encoded data in bytes, **without** its selector.
-    fn data_size(&self) -> usize;
+    /// The size of the encoded body data in bytes.
+    fn body_size(&self) -> usize;
 
-    /// ABI decode this call's arguments from the given slice, **without** its
-    /// selector.
-    #[inline]
-    fn decode_raw(data: &[u8], validate: bool) -> Result<Self> {
-        <Self::Tuple as SolType>::decode(data, validate).map(Self::from_rust)
+    fn topic_0(&self) -> <Self::Topic0 as SolType>::RustType;
+    fn topic_1(&self) -> <Self::Topic1 as SolType>::RustType;
+    fn topic_2(&self) -> <Self::Topic2 as SolType>::RustType;
+    fn topic_3(&self) -> <Self::Topic3 as SolType>::RustType;
+}
+
+mod example {
+
+    use alloy_primitives::{keccak256, U256};
+
+    use crate::{sol_data, SolEvent, SolType};
+
+    //  event MyEvent(bytes32 indexed a, uint256 b, string indexed c, bytes d);
+    pub struct MyEvent {
+        pub a: [u8; 32],
+        pub b: U256,
+        pub c: String,
+        pub d: Vec<u8>,
     }
 
-    /// ABI decode this call's arguments from the given slice, **with** the
-    /// selector.
-    #[inline]
-    fn decode(data: &[u8], validate: bool) -> Result<Self> {
-        let data = data
-            .strip_prefix(&Self::SELECTOR)
-            .ok_or_else(|| crate::Error::type_check_fail_sig(data, Self::SIGNATURE))?;
-        Self::decode_raw(data, validate)
-    }
+    impl SolEvent for MyEvent {
+        type BodyTuple = (sol_data::Uint<256>, sol_data::String, sol_data::Bytes);
+        type BodyToken = (
+            <sol_data::Uint<256> as SolType>::TokenType,
+            <sol_data::String as SolType>::TokenType,
+            <sol_data::Bytes as SolType>::TokenType,
+        );
+        const SIGNATURE: &'static str = "MyEvent(bytes32,uint256,string,bytes)";
+        const SIGNATURE_HASH: [u8; 32] = [0; 32]; // FIXME
+        const ANONYMOUS: bool = false;
+        const TOPICS_LEN: usize = 3;
+        type Topic0 = sol_data::FixedBytes<32>; // Signature hash
+        type Topic1 = sol_data::FixedBytes<32>; // Indexed bytes32 a
+        type Topic2 = sol_data::FixedBytes<32>; // Hash of indexed string c
+        type Topic3 = (); // no 4th topic
 
-    /// ABI encode the call to the given buffer **without** its selector.
-    #[inline]
-    fn encode_raw(&self, out: &mut Vec<u8>) {
-        out.reserve(self.data_size());
-        out.extend(<Self::Tuple as SolType>::encode(self.to_rust()));
-    }
+        fn body_to_rust(&self) -> <Self::BodyTuple as SolType>::RustType {
+            (self.b, self.c.clone(), self.d.clone())
+        }
 
-    /// ABI encode the call to the given buffer **with** its selector.
-    #[inline]
-    fn encode(&self) -> Vec<u8> {
-        let mut out = Vec::with_capacity(4 + self.data_size());
-        out.extend(&Self::SELECTOR);
-        self.encode_raw(&mut out);
-        out
+        fn from_rust(
+            topic0: <Self::Topic0 as SolType>::RustType,
+            topic1: <Self::Topic1 as SolType>::RustType,
+            topic2: <Self::Topic2 as SolType>::RustType,
+            topic3: <Self::Topic3 as SolType>::RustType,
+            body: <Self::BodyTuple as SolType>::RustType,
+        ) -> Self {
+            Self {
+                a: topic1,
+                b: body.0,
+                c: body.1,
+                d: body.2,
+            }
+        }
+
+        fn body_size(&self) -> usize {
+            0
+            // FIXME: as data_size for error.
+        }
+
+        fn topic_0(&self) -> <Self::Topic0 as SolType>::RustType {
+            Self::SIGNATURE_HASH
+        }
+
+        fn topic_1(&self) -> <Self::Topic1 as SolType>::RustType {
+            self.a
+        }
+
+        fn topic_2(&self) -> <Self::Topic2 as SolType>::RustType {
+            keccak256(self.c.as_bytes()).into()
+        }
+
+        fn topic_3(&self) -> <Self::Topic3 as SolType>::RustType {
+            ()
+        }
     }
 }
