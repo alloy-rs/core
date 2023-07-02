0328335fe1c0a210207da844b0e0ee313502d6d2
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-07-02 17:04:50 +0200

    test: update tests

--- crates/json-abi/tests/test.rs
+++ crates/json-abi/tests/test.rs
@@ -45,10 +45,9 @@ abi_parse_tests! {
     large_struct("abi/LargeStruct.json", 1)
     large_structs("abi/LargeStructs.json", 4)
     large_tuple("abi/LargeTuple.json", 1)
-    seaport("abi/Seaport.json", 67)
+    seaport("abi/Seaport.json", 69)
 }
 
-#[track_caller]
 fn parse_test(s: &str, len: usize) {
     let abi_items: Vec<AbiItem<'_>> = serde_json::from_str(s).unwrap();
     assert_eq!(abi_items.len(), len);
@@ -66,10 +65,11 @@ fn parse_test(s: &str, len: usize) {
     assert_eq!(abi2, abi3);
 
     iterator_test(abi1.items(), abi1.items().rev(), len);
+    iterator_test(abi1.items().skip(1), abi1.items().skip(1).rev(), len - 1);
     iterator_test(abi1.clone().into_items(), abi1.into_items().rev(), len);
 }
 
-fn iterator_test<T, I, R>(mut items: I, rev: R, len: usize)
+fn iterator_test<T, I, R>(items: I, rev: R, len: usize)
 where
     T: PartialEq + std::fmt::Debug,
     I: Iterator<Item = T> + DoubleEndedIterator + ExactSizeIterator,
@@ -78,12 +78,7 @@ where
     assert_eq!(items.len(), len);
     assert_eq!(items.size_hint(), (len, Some(len)));
 
-    items.next();
-    assert_eq!(items.len(), len - 1);
-    assert_eq!(items.size_hint(), (len - 1, Some(len - 1)));
-
     let mut items2: Vec<_> = items.collect();
-    assert_eq!(items2.len(), len);
     items2.reverse();
-    assert_eq!(items2, rev.skip(1).collect::<Vec<_>>());
+    assert_eq!(items2, rev.collect::<Vec<_>>());
 }
