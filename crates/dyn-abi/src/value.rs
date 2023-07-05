5ca9e1ff3cfa9691e39b68c65985dbe511f47595
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-07-06 01:30:45 +0200

    chore: clippy

--- crates/dyn-abi/src/value.rs
+++ crates/dyn-abi/src/value.rs
@@ -191,6 +191,7 @@ impl DynSolValue {
     }
 
     #[inline]
+    #[allow(clippy::missing_const_for_fn)]
     fn sol_type_name_simple(&self) -> Option<&str> {
         match self {
             Self::Address(_) => Some("address"),
