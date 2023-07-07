f8f3612f1de508f3c13350940e89a5cdd0671123
James <james@prestwi.ch>
2023-07-07 15:18:56 -0700

    docs: update some

--- crates/json-abi/src/param.rs
+++ crates/json-abi/src/param.rs
@@ -17,11 +17,16 @@ use crate::utils::{validate_identifier, validate_ty};
 /// [Error]: crate::Error
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct Param {
-    /// The name of the parameter.
+    /// The name of the parameter. This field always contains either the empty
+    /// string, or a valid Solidity identifier.
     pub name: String,
-    /// The canonical Solidity type of the parameter.
+    /// The canonical Solidity type of the parameter, using the word "tuple" to
+    /// represent complex types. E.g. `uint256` or `bytes[2]` or `tuple` or
+    /// `tuple[2]`. This field always contains a valid
+    /// [`alloy_sol_type_str::TypeSpecifier`].
     pub ty: String,
-    /// A list of the parameter's components, in order.
+    /// If the paramaeter is a compound type (a struct or tuple), a list of the
+    /// parameter's components, in order. Empty otherwise
     pub components: Vec<Param>,
     /// The internal type of the parameter. This type represents the type that
     /// the author of the solidity contract specified. E.g. for a contract, this
@@ -107,15 +112,22 @@ impl Param {
 /// `indexed` field.
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct EventParam {
-    /// The name of the parameter.
+    /// The name of the parameter. If this parameter is a struct, this field
+    /// will contain the Struct's name in the solidity contract. This field
+    /// always contains either the empty string, or a valid Solidity identifier.
     pub name: String,
-    /// The canonical Solidity type of the parameter.
+    /// The canonical Solidity type of the parameter, using the word "tuple" to
+    /// represent complex types. E.g. `uint256` or `bytes[2]` or `tuple` or
+    /// `tuple[2]`. This field always contains a valid
+    /// [`alloy_sol_type_str::TypeSpecifier`].
     pub ty: String,
     /// Whether the parameter is indexed. Indexed parameters have their
     /// value, or the hash of their value, stored in the log topics.
     pub indexed: bool,
-    /// A list of the parameter's components, in order. This is a tuple
-    /// definition, and sub-components will NOT have an `indexed` field.
+    /// If the paramaeter is a compound type (a struct or tuple), a list of the
+    /// parameter's components, in order. Empty otherwise. Because the
+    /// components are not top-level event params, they will not have an
+    /// `indexed` field.
     pub components: Vec<Param>,
     /// The internal type of the parameter. This type represents the type that
     /// the author of the solidity contract specified. E.g. for a contract, this
