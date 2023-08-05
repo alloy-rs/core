5f39812309e57f9ed2db39740243c718b5b5840b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-08-05 18:31:15 +0200

    fix(sol-macro): snake_case'd function names (#226)
    * fix(sol-macro): snake_case'd function names
    
    * chore: clippy

--- crates/sol-macro/src/expand/contract.rs
+++ crates/sol-macro/src/expand/contract.rs
@@ -386,7 +386,7 @@ fn generate_variant_conversions(name: &Ident, variant: &Ident, ty: &Ident) -> To
 
 fn generate_variant_methods((variant, ty): (&Ident, &Ident)) -> TokenStream {
     let name = variant.unraw();
-    let name_snake = name.to_string().to_snake_case();
+    let name_snake = snakify(&name.to_string());
 
     let is_variant = format_ident!("is_{name_snake}");
     let is_variant_doc = format!("Returns `true` if `self` matches [`{name}`](Self::{name}).");
@@ -426,4 +426,23 @@ fn generate_variant_methods((variant, ty): (&Ident, &Ident)) -> TokenStream {
             }
         }
     }
+}
+
+/// `heck` doesn't treat numbers as new words, and discards leading underscores.
+fn snakify(s: &str) -> String {
+    let leading = s.chars().take_while(|c| *c == '_');
+    let mut output: Vec<char> = leading.chain(s.to_snake_case().chars()).collect();
+
+    let mut num_starts = vec![];
+    for (pos, c) in output.iter().enumerate() {
+        if pos != 0 && c.is_ascii_digit() && !output[pos - 1].is_ascii_digit() {
+            num_starts.push(pos);
+        }
+    }
+    // need to do in reverse, because after inserting, all chars after the point of
+    // insertion are off
+    for i in num_starts.into_iter().rev() {
+        output.insert(i, '_')
+    }
+    output.into_iter().collect()
 }
