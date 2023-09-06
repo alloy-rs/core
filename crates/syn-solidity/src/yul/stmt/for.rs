cf5ee38ea24adbc674bf4b07c9b95c6a524e8293
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2023-09-06 20:47:32 +0300

    feat(syn-solidity): support parsing yul for loops

---
+++ crates/syn-solidity/src/yul/stmt/for.rs
@@ -0,0 +1,61 @@
+use proc_macro2::Span;
+use std::fmt;
+use syn::{
+    parse::{Parse, ParseStream},
+    Result, Token,
+};
+
+use crate::{yul::expr::YulExpr, Spanned, YulBlock};
+
+// Yul for loop e.g `for {let i := 0} lt(i,10) {i := add(i,1)} {mstore(i,7)}`
+//
+// Solidity Reference:
+// <https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityParser.yulForStatement>
+// breakdown of parts: <https://docs.soliditylang.org/en/latest/yul.html#loops>
+#[derive(Clone)]
+pub struct YulFor {
+    for_token: Token![for],
+    initialization: YulBlock,
+    condition: YulExpr,
+    post_iteration: YulBlock,
+    body: YulBlock,
+}
+
+impl Parse for YulFor {
+    fn parse(input: ParseStream<'_>) -> Result<Self> {
+        Ok(Self {
+            for_token: input.parse()?,
+            initialization: input.parse()?,
+            condition: input.parse()?,
+            post_iteration: input.parse()?,
+            body: input.parse()?,
+        })
+    }
+}
+
+impl fmt::Debug for YulFor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("YulFor")
+            .field("for_token", &self.for_token)
+            .field("initialization", &self.initialization)
+            .field("condition", &self.condition)
+            .field("post_iteration", &self.post_iteration)
+            .field("body", &self.body)
+            .finish()
+    }
+}
+
+impl Spanned for YulFor {
+    fn span(&self) -> Span {
+        let span = self.for_token.span();
+        span.join(self.body.span()).unwrap_or(span)
+    }
+
+    fn set_span(&mut self, span: Span) {
+        self.for_token.set_span(span);
+        self.initialization.set_span(span);
+        self.condition.set_span(span);
+        self.post_iteration.set_span(span);
+        self.body.set_span(span);
+    }
+}
