fb5fcc26a14f297652a6e7edff395be350544d62
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-09-25 16:30:48 +0200

    chore: sync crate level attributes (#303)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -4,12 +4,14 @@
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
 #![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
     missing_docs,
     unreachable_pub,
-    unused_crate_dependencies,
     clippy::missing_const_for_fn,
     rustdoc::all
 )]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
--- crates/sol-macro/src/lib.rs
+++ crates/sol-macro/src/lib.rs
@@ -12,7 +12,13 @@
     html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
-#![warn(missing_docs, rustdoc::all)]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
