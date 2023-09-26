130ed395eea988a4ad1b59d1b9112b3f68b3c290
alessandromazza <alessandro98.mazza@gmail.com>
2023-09-26 16:24:12 +0200

    fix function and use core instead of std library

--- crates/sol-types/src/types/error.rs
+++ crates/sol-types/src/types/error.rs
@@ -411,21 +411,14 @@ impl PanicKind {
 /// Returns the revert reason from the given output data, if it's an abi encoded
 /// String. Returns `None` if the output is not long enough to contain a
 /// function selector or the content is not a valid abi encoded String.
-///
-/// **Note:** it's assumed the `out` buffer starts with the call's signature
 pub fn decode_revert_reason(out: &[u8]) -> Option<String> {
-    // Ensure the output data is long enough to contain a function selector.
-    if out.len() < Selector::len_bytes() {
-        return None
-    }
-
     // Try to decode as a generic contract error.
     if let Ok(error) = GenericContractError::decode(out, true) {
         return Some(error.to_string())
     }
 
     // If that fails, try to decode as a regular string.
-    if let Ok(decoded_string) = std::str::from_utf8(out) {
+    if let Ok(decoded_string) = core::str::from_utf8(out) {
         return Some(decoded_string.to_string())
     }
 
