6a3fa21fc3c9f2989095996d7212ce27555e2d4d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-09-29 03:46:07 +0200

    test(syn-solidity): improve contract tests (#316)
    * chore: move patching to a struct
    
    * test: rewrite assertions with a visitor

--- crates/syn-solidity/tests/contracts.rs
+++ crates/syn-solidity/tests/contracts.rs
@@ -1,5 +1,12 @@
-use std::{fs, path::Path, process::Command};
-use syn_solidity::{File, Item};
+use std::{
+    fs::{self, DirEntry},
+    path::Path,
+    process::Command,
+};
+use syn_solidity::{
+    visit::{self, Visit},
+    File,
+};
 
 #[test]
 fn contracts() {
@@ -10,75 +17,123 @@ fn contracts() {
         .parent()
         .unwrap();
 
-    let mut files: Vec<_> = fs::read_dir(PATH)
+    let mut entries: Vec<_> = fs::read_dir(PATH)
         .unwrap()
         .collect::<Result<_, _>>()
         .unwrap();
-    files.sort_by_key(std::fs::DirEntry::path);
-    let patches = files
-        .iter()
-        .filter(|p| p.path().extension() == Some("patch".as_ref()));
-    let files = files
-        .iter()
-        .filter(|p| p.path().extension() == Some("sol".as_ref()));
-
-    for patch in patches.clone() {
-        let s = Command::new("git")
-            .current_dir(root)
-            .arg("apply")
-            .arg(patch.path())
-            .status()
-            .unwrap();
-        assert!(s.success(), "failed to apply patch: {s}");
-    }
-
-    let mut failed = false;
-    for file in files {
+    entries.sort_by_key(std::fs::DirEntry::path);
+    let mut patcher = GitPatcher::new(entries, root);
+    patcher.patch();
+    for file in patcher.files() {
         let path = file.path();
-        let name = path.file_name().unwrap().to_str().unwrap();
-
-        match std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| parse_file(&path))) {
-            Ok(Ok(())) => {}
-            Ok(Err(e)) => {
-                eprintln!("failed to parse {name}: {e} ({e:?})");
-                failed = true;
+        let file = match parse_file(&path) {
+            Ok(file) => file,
+            Err(e) => {
+                panic!("failed to parse {}: {e}", path.display())
             }
-            Err(_) => {
-                eprintln!("panicked while parsing {name}");
-                failed = true;
-            }
-        }
-        if failed {
-            break
-        }
+        };
+        eprintln!("visiting {}", path.display());
+        TestVisitor.visit_file(&file);
     }
-
-    for patch in patches {
-        let s = Command::new("git")
-            .current_dir(root)
-            .arg("apply")
-            .arg("--reverse")
-            .arg(patch.path())
-            .status()
-            .unwrap();
-        assert!(s.success(), "failed to reset patch: {s}");
-    }
-
-    assert!(!failed);
+    patcher.unpatch();
 }
 
-fn parse_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+/// Runs `unpatch` on drop. This ensures that the patch is always reset even if
+/// the test panics.
+struct GitPatcher<'a> {
+    entries: Vec<DirEntry>,
+    root: &'a Path,
+    patched: bool,
+}
+
+impl<'a> GitPatcher<'a> {
+    fn new(entries: Vec<DirEntry>, root: &'a Path) -> Self {
+        Self {
+            entries,
+            root,
+            patched: false,
+        }
+    }
+
+    fn patches(&self) -> impl Iterator<Item = &DirEntry> {
+        self.entries
+            .iter()
+            .filter(|p| p.path().extension() == Some("patch".as_ref()))
+    }
+
+    fn files(&self) -> impl Iterator<Item = &DirEntry> {
+        self.entries
+            .iter()
+            .filter(|p| p.path().extension() == Some("sol".as_ref()))
+    }
+
+    fn patch(&mut self) {
+        self.patched = true;
+        for patch in self.patches() {
+            let path = patch.path();
+            let s = Command::new("git")
+                .current_dir(self.root)
+                .arg("apply")
+                .arg(&path)
+                .status()
+                .unwrap();
+            assert!(
+                s.success(),
+                "failed to apply patch at {}: {s}",
+                path.display()
+            );
+        }
+    }
+
+    fn unpatch(&mut self) {
+        if !self.patched {
+            return
+        }
+        self.patched = false;
+        for patch in self.patches() {
+            let path = patch.path();
+            match Command::new("git")
+                .current_dir(self.root)
+                .arg("apply")
+                .arg("--reverse")
+                .arg(&path)
+                .status()
+            {
+                Ok(s) if s.success() => {}
+                e => {
+                    eprintln!("failed to reset patch at {}: {e:?}", path.display())
+                }
+            }
+        }
+    }
+}
+
+impl Drop for GitPatcher<'_> {
+    fn drop(&mut self) {
+        self.unpatch();
+    }
+}
+
+fn parse_file(path: &Path) -> Result<File, Box<dyn std::error::Error>> {
     let solidity = fs::read_to_string(path)?;
-    let file: File = syn::parse_str(&solidity)?;
-    assert!(!file.items.is_empty());
-    file.items.iter().try_for_each(assert_item)
+    syn::parse_str(&solidity).map_err(Into::into)
 }
 
-fn assert_item(item: &Item) -> Result<(), Box<dyn std::error::Error>> {
-    match item {
-        Item::Contract(contract) => contract.body.iter().try_for_each(assert_item),
-        Item::Enum(e) if e.variants.is_empty() => Err("empty enum".into()),
-        Item::Struct(s) if s.fields.is_empty() => Err("empty struct".into()),
-        _ => Ok(()),
+struct TestVisitor;
+
+impl<'ast> Visit<'ast> for TestVisitor {
+    fn visit_file(&mut self, file: &'ast File) {
+        assert!(!file.items.is_empty());
+        visit::visit_file(self, file);
+    }
+
+    fn visit_item_enum(&mut self, e: &'ast syn_solidity::ItemEnum) {
+        assert!(!e.variants.is_empty());
+        visit::visit_item_enum(self, e);
+    }
+
+    fn visit_item_struct(&mut self, s: &'ast syn_solidity::ItemStruct) {
+        assert!(!s.fields.is_empty());
+        visit::visit_item_struct(self, s);
     }
 }
