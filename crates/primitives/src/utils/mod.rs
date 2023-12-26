5af8a9a22d7deaef4bf85a35600413109d04a718
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-12-26 22:54:25 +0100

    fmt

--- crates/primitives/src/utils/mod.rs
+++ crates/primitives/src/utils/mod.rs
@@ -85,7 +85,7 @@ pub fn keccak256<T: AsRef<[u8]>>(bytes: T) -> B256 {
                 // SAFETY: The output is 32-bytes, and the input comes from a slice.
                 unsafe { native_keccak256(bytes.as_ptr(), bytes.len(), output.as_mut_ptr().cast()) };
             } else if #[cfg(feature = "asm-keccak")] {
-                use keccak_asm::{Keccak256,digest::Digest};
+                use keccak_asm::{digest::Digest, Keccak256};
 
                 let mut hasher = Keccak256::new();
                 hasher.update(bytes);
