50ebf49da5c8dcfc765267af4a4049119f4d77ab
Enrique <hi@enriqueortiz.dev>
2024-01-25 12:30:16 -0400

    Update crates/primitives/src/signature/parity.rs
    Co-authored-by: Bjerg <onbjerg@users.noreply.github.com>

--- crates/primitives/src/signature/parity.rs
+++ crates/primitives/src/signature/parity.rs
@@ -84,7 +84,7 @@ impl Parity {
     }
 
     /// Return the y-parity byte as 27 or 28,
-    /// In the case of a non-EIP155 signature.
+    /// in the case of a non-EIP155 signature.
     pub const fn y_parity_byte_non_eip155(&self) -> Option<u8> {
         match self {
             Self::NonEip155(v) => Some(*v as u8 + 27),
