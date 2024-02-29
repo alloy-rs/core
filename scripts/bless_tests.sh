3110381c8b7957b8a48374c6c96bb7435d0436fa
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-02-29 17:37:54 +0100

    test: bless tests

---
+++ scripts/bless_tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+export RUSTFLAGS="-Zthreads=1"
+export TRYBUILD=overwrite
+cargo +nightly test -p alloy-sol-types --test compiletest
+cargo +nightly test -p alloy-sol-types --test compiletest --all-features
