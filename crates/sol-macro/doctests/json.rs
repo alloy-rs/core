08edabb0414388083374666ff9d18c6039d128cb
Arsenii Kulikov <klkvrr@gmail.com>
2024-07-23 16:31:58 +0800

    add test

--- crates/sol-macro/doctests/json.rs
+++ crates/sol-macro/doctests/json.rs
@@ -36,7 +36,63 @@ sol! {
     }
 }
 
+sol!(
+    ContractWithDuplicates1,
+    r#"[
+        {
+            "inputs": [
+                {
+                    "name": "aValue_",
+                    "type": "tuple",
+                    "internalType": "struct LibA.Struct",
+                    "components": [
+                        {
+                            "name": "field64",
+                            "type": "uint64",
+                            "internalType": "uint64"
+                        }
+                    ]
+                },
+                {
+                    "name": "bValue_",
+                    "type": "tuple",
+                    "internalType": "struct LibB.Struct",
+                    "components": [
+                        {
+                            "name": "field128",
+                            "type": "uint128",
+                            "internalType": "uint128"
+                        }
+                    ]
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "view",
+            "name": "foo",
+            "type": "function"
+        }
+    ]"#
+);
+
+// This is the same as:
+sol! {
+    interface ContractWithDuplicates2 {
+        struct Struct0 {
+            uint64 field64;
+        }
+        struct Struct1 {
+            uint128 field128;
+        }
+
+        function foo(Struct0 aValue_, Struct1 bValue_) external view;
+    }
+}
+
 #[test]
 fn abigen() {
     assert_eq!(MyJsonContract1::fooCall::SIGNATURE, MyJsonContract2::fooCall::SIGNATURE,);
+    assert_eq!(
+        ContractWithDuplicates1::fooCall::SIGNATURE,
+        ContractWithDuplicates2::fooCall::SIGNATURE,
+    );
 }
