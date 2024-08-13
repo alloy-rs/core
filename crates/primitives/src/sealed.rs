18fbaebb7c9cee337f9737a55da3758558a96e15
Roman Krasiuk <rokrassyuk@gmail.com>
2024-08-13 15:57:09 -0700

    feat: derive ser deser on `Sealed`

--- crates/primitives/src/sealed.rs
+++ crates/primitives/src/sealed.rs
@@ -5,6 +5,7 @@ use crate::B256;
 /// We do not implement any specific hashing algorithm here. Instead types
 /// implement the [`Sealable`] trait to provide define their own hash.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Sealed<T> {
     /// The inner item
     inner: T,
