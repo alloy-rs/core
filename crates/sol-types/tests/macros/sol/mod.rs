defe556a5b1b7202d1f78b54239cb351978ed550
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-08-21 04:07:08 +0200

    Revert "chore(deps): bump derive_more to 1.0"
    This reverts commit 81f8f461681fc7d31fedf2b68203a5595f21e6cc.

--- crates/sol-types/tests/macros/sol/mod.rs
+++ crates/sol-types/tests/macros/sol/mod.rs
@@ -720,70 +720,70 @@ fn duplicate_events() {
     sol! {
     #[derive(derive_more::Display)]
     interface Console {
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log(string val);
 
-        #[display("{}", "hex::encode_prefixed(val)")]
+        #[display(fmt = "{}", "hex::encode_prefixed(val)")]
         event logs(bytes val);
 
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log_address(address val);
 
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log_bytes32(bytes32 val);
 
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log_int(int val);
 
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log_uint(uint val);
 
-        #[display("{}", "hex::encode_prefixed(val)")]
+        #[display(fmt = "{}", "hex::encode_prefixed(val)")]
         event log_bytes(bytes val);
 
-        #[display("{val}")]
+        #[display(fmt = "{val}")]
         event log_string(string val);
 
-        #[display("{val:?}")]
+        #[display(fmt = "{val:?}")]
         event log_array(uint256[] val);
 
-        #[display("{val:?}")]
+        #[display(fmt = "{val:?}")]
         event log_array(int256[] val);
 
-        #[display("{val:?}")]
+        #[display(fmt = "{val:?}")]
         event log_array(address[] val);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_address(string key, address val);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_bytes32(string key, bytes32 val);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_decimal_int(string key, int val, uint decimals);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_decimal_uint(string key, uint val, uint decimals);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_int(string key, int val);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_uint(string key, uint val);
 
-        #[display("{key}: {val:?}")]
+        #[display(fmt = "{key}: {val:?}")]
         event log_named_bytes(string key, bytes val);
 
-        #[display("{key}: {val}")]
+        #[display(fmt = "{key}: {val}")]
         event log_named_string(string key, string val);
 
-        #[display("{key}: {val:?}")]
+        #[display(fmt = "{key}: {val:?}")]
         event log_named_array(string key, uint256[] val);
 
-        #[display("{key}: {val:?}")]
+        #[display(fmt = "{key}: {val:?}")]
         event log_named_array(string key, int256[] val);
 
-        #[display("{key}: {val:?}")]
+        #[display(fmt = "{key}: {val:?}")]
         event log_named_array(string key, address[] val);
     }
     }
