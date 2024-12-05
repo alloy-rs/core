f382c1873d11c54a773a13d07953ee175ba70863
moricho <ikeda.morito@gmail.com>
2024-12-05 19:33:45 +0900

    chore: mark Signature as `deprecated`

--- crates/primitives/src/signature/mod.rs
+++ crates/primitives/src/signature/mod.rs
@@ -2,9 +2,11 @@ mod error;
 pub use error::SignatureError;
 
 mod parity;
+#[deprecated(since = "0.8.15", note = "see https://github.com/alloy-rs/core/pull/776")]
 pub use parity::Parity;
 
 mod sig;
+#[deprecated(since = "0.8.15", note = "use PrimitiveSignature instead")]
 pub use sig::Signature;
 
 mod utils;
