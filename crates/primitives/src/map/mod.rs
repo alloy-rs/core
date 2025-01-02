06dba68add833b557a9f6d339a70fc21d1aa2098
lightsing <light.tsing@gmail.com>
2025-01-02 16:28:57 +0800

    merge imports

--- crates/primitives/src/map/mod.rs
+++ crates/primitives/src/map/mod.rs
@@ -127,14 +127,15 @@ pub mod rayon {
 
     cfg_if! {
         if #[cfg(any(feature = "map-hashbrown", not(feature = "std")))] {
-            pub use hashbrown::hash_map::rayon::IntoParIter as IntoIter;
-            pub use hashbrown::hash_map::rayon::ParDrain as Drain;
-            pub use hashbrown::hash_map::rayon::ParIter as Iter;
-            pub use hashbrown::hash_map::rayon::ParIterMut as IterMut;
-            pub use hashbrown::hash_map::rayon::ParKeys as Keys;
-            pub use hashbrown::hash_map::rayon::ParValues as Values;
-            pub use hashbrown::hash_map::rayon::ParValuesMut as ValuesMut;
-
+            pub use hashbrown::hash_map::rayon::{
+                IntoParIter as IntoIter,
+                ParDrain as Drain,
+                ParIter as Iter,
+                ParIterMut as IterMut,
+                ParKeys as Keys,
+                ParValues as Values,
+                ParValuesMut as ValuesMut
+            };
             use ::rayon as _;
         } else {
             pub use ::rayon::collections::hash_map::*;
