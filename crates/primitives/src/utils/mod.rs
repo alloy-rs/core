a74d394ca7dafa63b82cdc3da6c490c06e191590
zerosnacks <zerosnacks@protonmail.com>
2025-04-17 15:13:47 +0200

    style: add 0x prefix

--- crates/primitives/src/utils/mod.rs
+++ crates/primitives/src/utils/mod.rs
@@ -26,7 +26,7 @@ pub const EIP191_PREFIX: &str = "\x19Ethereum Signed Message:\n";
 
 /// The [Keccak-256](keccak256) hash of the empty string `""`.
 pub const KECCAK256_EMPTY: B256 =
-    b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+    b256!("0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
 
 /// Tries to create a [`Vec`] containing the arguments.
 #[macro_export]
