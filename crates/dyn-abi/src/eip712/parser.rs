4149cc65177418c6d05bcdfba534c50eda56a90f
0xrusowsky <90208954+0xrusowsky@users.noreply.github.com>
2025-05-19 16:30:21 +0200

    fix: use ok_or_else
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/dyn-abi/src/eip712/parser.rs
+++ crates/dyn-abi/src/eip712/parser.rs
@@ -156,7 +156,7 @@ impl<'a> EncodeType<'a> {
         // Resolve and validate non-dependent types
         let mut non_dependent = resolver.non_dependent_types();
 
-        let first = non_dependent.next().ok_or(Error::MissingType("primary component".into()))?;
+        let first = non_dependent.next().ok_or_else(|| Error::MissingType("primary component".into()))?;
         if let Some(second) = non_dependent.next() {
             let all_types = vec![first.type_name(), second.type_name()]
                 .into_iter()
