f7ca2a14b15b4d448b16bcd54e1757e3b9ece8c2
0xrusowsky <90208954+0xrusowsky@users.noreply.github.com>
2025-06-16 10:56:51 +0200

    fmt: wording
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- crates/sol-type-parser/src/type_spec.rs
+++ crates/sol-type-parser/src/type_spec.rs
@@ -115,7 +115,7 @@ impl<'a> TypeSpecifier<'a> {
 
     /// Parse a type specifier from a string.
     ///
-    /// Despite EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
+    /// Although EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
     /// uses colons for their EIP-712 namespacing, alloy decided to add support for them. See the relevant discussion at: [`https://github.com/foundry-rs/foundry/issues/10765`].
     #[cfg(feature = "eip712")]
     #[inline]
