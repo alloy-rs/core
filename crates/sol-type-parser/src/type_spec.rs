713cbcdbf90e473f6efe50b3c68f0684aa2d9564
0xrusowsky <0xrusowsky@proton.me>
2025-06-16 10:57:56 +0200

    docs: wording

--- crates/sol-type-parser/src/type_spec.rs
+++ crates/sol-type-parser/src/type_spec.rs
@@ -115,8 +115,8 @@ impl<'a> TypeSpecifier<'a> {
 
     /// Parse a type specifier from a string.
     ///
-    /// Despite EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
-    /// uses colons for their EIP-712 namespacing, alloy decided to add support for them. See the relevant discussion at: [`https://github.com/foundry-rs/foundry/issues/10765`].
+    /// Although EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
+    /// uses colons for their EIP-712 namespacing, alloy decided to add support for them. See the relevant discussion at: [#10765](https://github.com/foundry-rs/foundry/issues/10765).
     #[cfg(feature = "eip712")]
     #[inline]
     pub fn parse_eip712(s: &'a str) -> Result<Self> {
@@ -125,8 +125,8 @@ impl<'a> TypeSpecifier<'a> {
 
     /// [`winnow`] parser for EIP-712 types (allows colons in identifiers).
     ///
-    /// Despite EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
-    /// uses colons for their namespacing, alloy decided to add support for them. See the relevant discussion at: [`https://github.com/foundry-rs/foundry/issues/10765`].
+    /// Although EIP-712 identifiers should stick to the [solidity type naming spec](https://docs.soliditylang.org/en/latest/grammar.html#a4.SolidityLexer.Identifier), since Hyperliquid
+    /// uses colons for their namespacing, alloy decided to add support for them. See the relevant discussion at: [#10765](https://github.com/foundry-rs/foundry/issues/10765).
     #[cfg(feature = "eip712")]
     pub(crate) fn eip712_parser(input: &mut Input<'a>) -> ModalResult<Self> {
         trace(
