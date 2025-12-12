504e986f20bcfc1a238745d7589da089a662e4d3
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-12-12 20:50:00 -0300

    chore: rm all deprecations (#1048)
    * chore: rm all deprecations
    
    * docs

--- crates/primitives/src/signature/mod.rs
+++ crates/primitives/src/signature/mod.rs
@@ -6,7 +6,3 @@ pub use utils::{normalize_v, to_eip155_v};
 
 mod sig;
 pub use sig::Signature;
-
-/// Deprecated alias for [`Signature`].
-#[deprecated(since = "0.9.0", note = "use Signature instead")]
-pub type PrimitiveSignature = Signature;
