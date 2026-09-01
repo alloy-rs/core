db78603d9a54cab7b2a0783ea91b165bd32dc94e
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-09-01 18:06:58 +0200

    refactor(sol-types): simplify bool validation

--- crates/sol-types/src/types/data_type.rs
+++ crates/sol-types/src/types/data_type.rs
@@ -49,7 +49,7 @@ impl SolType for Bool {
 
     #[inline]
     fn valid_token(token: &Self::Token<'_>) -> bool {
-        token.0 == Word::ZERO || token.0 == Word::with_last_byte(1)
+        utils::check_zeroes(&token.0[..31]) && token.0[31] <= 1
     }
 
     #[inline]
